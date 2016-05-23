00547802fcab8f9bbdbc4d82ade3d24461b0196a
Lukas Joergensen <lukaspj@outlook.com>
2016-05-23 20:57:54 +0200

    Removed negative let-test from examples

--- exploration/scheme/exampels.scm
+++ exploration/scheme/exampels.scm
@@ -15,15 +15,7 @@
 (define test4
   (label-expression '(label (integrity . 0) ())
 		    test3))
-
-
-(define test5
-  (label-expression '(label (integrity . 1) ())
-                    (let ([let-test1 test3])
-                      let-test1)))
     
-
-
 (define verify-exampels
   (lambda ()
     (verify-constraints "exampels.scm")))
