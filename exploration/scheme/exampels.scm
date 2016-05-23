0105bd11fff408964332b37eb0b0cdf25485785e
Lynderup <lynderupjensen@gmail.com>
2016-05-23 19:20:47 +0200

    Added an example

--- exploration/scheme/exampels.scm
+++ exploration/scheme/exampels.scm
@@ -1,6 +1,5 @@
 (load "framework.scm")
 
-(principals 'alice 'bob)
 
 (define test1
   (label-expression '(label (integrity . 0) (confidentiality . 0))
@@ -14,8 +13,15 @@
   test1)
 
 (define test4
-  (label-expression '(label (integrity . 1) ())
+  (label-expression '(label (integrity . 0) ())
 		    test3))
+
+
+(define test5
+  (label-expression '(label (integrity . 1) ())
+                    (let ([let-test1 test3])
+                      let-test1)))
+    
 
 
 (define verify-exampels
