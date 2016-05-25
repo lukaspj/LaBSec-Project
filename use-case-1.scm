19cdfe4e1f83ede6d7d1d6c42d15ff072e7be8aa
Lukas Joergensen <lukaspj@outlook.com>
2016-05-25 15:27:08 +0200

    Rewrote to label-lambdas

--- use-case-1.scm
+++ use-case-1.scm
@@ -2,26 +2,64 @@
 
 ; Open-web-page -> hash(declassify) -> log-in -> download-data -> store-data
 
+;; (define fetch-data-from-web-page
+;;   (lambda (username password)
+;;     (store-data
+;;      "s-stuff.png"
+;;      (download-data
+;;       "strange-stuff.png"
+;;       (log-in username (hash password))))))
+
+;; (define download-data
+;;   (lambda (file access-token)
+;;     "Some data array"))
+
+;; (define log-in
+;;   (lambda (username passwordhash)
+;;     '(auth 1002)))
+
+;; (define hash
+;;   (lambda (v)
+;;     v))
+
+;; (define store-data
+;;   (lambda (filename data)
+;;     (printf "~s: ~s ~n" filename data)))
+
 (define fetch-data-from-web-page
-  (lambda (username password)
-    (store-data
-     "s-stuff.png"
-     (download-data
-      "strange-stuff.png"
-      (log-in username (hash password))))))
+  (label-lambda '(label () ())
+                ([username '(label () ())]
+                 [password '(label () ())])
+                '(label () ())
+                (store-data
+                 "s-stuff.png"
+                 (download-data
+                  "strange-stuff.png"
+                  (log-in username (hash password))))))
 
 (define download-data
-  (lambda (file access-token)
-    "Some data array"))
+  (label-lambda '(label () ())
+                ([file '(label () ())]
+                 [access-token '(label () ())])
+                '(label () ())
+                "Some data array"))
 
 (define log-in
-  (lambda (username passwordhash)
-    '(auth 1002)))
+  (label-lambda '(label () ())
+                ([username '(label () ())]
+                 [password '(label () ())])
+                '(label () ())
+                '(auth 1002)))
 
 (define hash
-  (lambda (v)
-    v))
+  (label-lambda '(label () ())
+                ([v '(label () ())])
+                '(label () ())
+                v))
 
 (define store-data
-  (lambda (filename data)
-    (printf "~s: ~s ~n" filename data)))
+  (label-lambda '(label () ())
+                ([filename '(label () ())]
+                 [data '(label () ())])
+                '(label () ())
+                (printf "~s: ~s ~n" filename data)))
